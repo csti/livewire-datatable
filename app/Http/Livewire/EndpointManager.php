91510c9ac7574f7171173aff0da52224eb5095b8
Cat Stefanovici <>
2020-01-26 13:16:09 -0500

    endpoint manager

---
+++ app/Http/Livewire/EndpointManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EndpointManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.endpoint-manager');
+    }
+}
