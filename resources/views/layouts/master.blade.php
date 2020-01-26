df553b3acb59f5d637c62c363b8211907b392ebe
Cat Stefanovici <>
2020-01-26 14:29:04 -0500

    tailwind and master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Information -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title', config('app.name'))</title>
+
+    <!-- Fonts -->
+
+    <!-- CSS -->
+    <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/pyloncss@latest/css/pylon.css"/>
+
+    <!-- Scripts -->
+    @yield('scripts', '')
+
+</head>
+<body>
+    <div id="app" class="bg-blue-100">
+        @yield('content')
+    </div>
+
+    <!-- JavaScript -->
+    @if(config('app.env') == 'local')
+    <script src="http://localhost:35729/livereload.js"></script>
+    @endif
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
